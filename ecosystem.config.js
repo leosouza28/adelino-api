6062f9c25187c8705aad6b27f0e38de80eb1c9a1
leonardosouza <leonardosouza@MacBook-Pro-de-leonardosouza.local>
2026-01-15 12:23:06 -0300

    leo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,16 +10,6 @@ module.exports = {
                 LOG_LEVEL: "EFI_WEBHOOK"
             },
             log_date_format: "DD/MM HH:mm"
-        },
-        {
-            name: "webhook-trackpix-sicoob",
-            script: "dist/webhook-server/webhook-server.js",
-            env: {
-                DEV: 0,
-                PORT: 3011,
-                LOG_LEVEL: "SICOOB_WEBHOOK"
-            },
-            log_date_format: "DD/MM HH:mm"
         }
     ]
 }
