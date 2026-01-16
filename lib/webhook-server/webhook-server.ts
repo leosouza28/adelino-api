967869c84c183d54e9acc0cda37958eb8374357d
leonardosouza <leonardosouza@MacBook-Pro-de-leonardosouza.local>
2026-01-16 10:25:48 -0300

    leo

--- lib/webhook-server/webhook-server.ts
+++ lib/webhook-server/webhook-server.ts
@@ -55,7 +55,7 @@ app.post("/pix/webhook", (request, response) => {
             response.status(401).end();
         }
     } catch (error) {
-        errorHandler(error, res);
+        errorHandler(error, response);
     }
 });
 
